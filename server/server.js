f6382801ca1eaeddf7fc9cc286c6fbf4a76deabd
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-03-10 11:57:07 -0600

    added get and post routes, html structure and client js structure

--- server/server.js
+++ server/server.js
@@ -3,7 +3,21 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+//server-side data: 
+//module for random number generator: 
 let rdmNum = require('./modules/random');
+//array to hold guesses
+let guesses = [];
+//array to hold results:
+let results = [];
+
+function compare(array, number){
+  array.forEach(item => {
+    // compares input guess with random number and pushes a boolean to result array
+    results.push(item === number);
+  });
+  return results;
+}
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -12,6 +26,20 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+
+// receives input guesses from client and adds them to guesses array
+app.post('/input', (req, res) => {
+  console.log('POST input', guesses.push(req.body));
+
+  res.sendStatus(201); // 201 -> Created
+});
+
+//get results route
+app.get('/input', function(req, res){
+//get results from comparison between user guesses POST and random number: 
+  console.log('getting inputs');
+res.send(results);
+});
 
 
 app.listen(PORT, () => {
